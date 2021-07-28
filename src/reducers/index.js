a1aa02357f391f72194a6ffa7343b55d240646e8
bg-dev <nik.azizoff2016@gmail.com>
2021-07-28 12:19:17 +0300

    change paths

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from "redux";
 
 const moviesReducer = () => {
     return[
-        {title: 'Spider-Man: Homecoming', releaseDate: '05-07-2017', rating: 7.4, path: "https://www.film.ru/sites/default/files/movies/posters/12563387-975943.jpg"},
-        {title: 'Aquaman', releaseDate: '12-07-2018', rating: 7, path: "https://i.pinimg.com/736x/17/98/cf/1798cfb2120fcef2b1907047006db6ff.jpg"},
-        {title: 'Black Panther', releaseDate: '02-13-2018', rating: 7.4, path: "https://kinocub.net/uploads/posts/2017-11/15103356248s.jpeg"},
-        {title: 'Avengers: Infinity War', releaseDate: '04-25-2018', rating: 8.3, path: "https://image.tmdb.org/t/p/original/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"},
-        {title: 'Guardians of the Galaxy', releaseDate: '07-30-2014', rating: 7.9, path: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/36436c19800817.5632caf2c8025.jpg"}
+        {title: 'Spider-Man: Homecoming', releaseDate: '05-07-2017', rating: 7.4, path: "./img/1.jpg"},
+        {title: 'Aquaman', releaseDate: '12-07-2018', rating: 7, path: "./img/2.jpg"},
+        {title: 'Black Panther', releaseDate: '02-13-2018', rating: 7.4, path: "./img/3.jpg"},
+        {title: 'Avengers: Infinity War', releaseDate: '04-25-2018', rating: 8.3, path: "./img/4.jpg"},
+        {title: 'Guardians of the Galaxy', releaseDate: '07-30-2014', rating: 7.9, path: "./img/5.jpg"}
     ]
 }
 
